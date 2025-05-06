661b75b9699fd717080985f702c015e8de0417df
Essa Sabbagh <50638748+essasabbagh@users.noreply.github.com>
2025-05-06 14:20:19 +0300

    Organize const

--- lib/const.dart
+++ lib/const.dart
@@ -5,16 +5,16 @@ const int port = 8080; // WebSocket port
 const String scheme = 'ws'; // 'ws' for non-secure connections
 
 /// For local development, web (chrom)
-const String host = '127.0.0.1'; // Your Mac's local IP
-const String baseUrl = 'http://127.0.0.1:8000/api';
-const String broadcastingUrl = 'http://127.0.0.1:8000/api/broadcasting/auth';
+// const String host = '127.0.0.1'; // Your Mac's local IP
+// const String baseUrl = 'http://127.0.0.1:8000/api';
+// const String broadcastingUrl = '$baseUrl/broadcasting/auth';
 
 // for mac local development
 // const String host = 'chat_app.test';
 // const String baseUrl = 'http://chat_app.test/api';
-// const String broadcastingUrl = 'http://chat_app.test/api/broadcasting/auth';
+// const String broadcastingUrl = '$baseUrl/broadcasting/auth';
 
 /// For local development, Android Emulator
-// const String host = '10.0.2.2';
-// const String baseUrl = 'http://10.0.2.2:8000/api';
-// const String broadcastingUrl = 'http://10.0.2.2:8000/api/broadcasting/auth';
+const String host = '10.0.2.2';
+const String baseUrl = 'http://10.0.2.2:8000/api';
+const String broadcastingUrl = '$baseUrl/broadcasting/auth';
